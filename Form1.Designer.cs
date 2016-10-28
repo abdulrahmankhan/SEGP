fc7bd3e6823067bb7a8ca688ee82ced695adeedb
Abdul_Rahman_Khan <rahman.khan803@hotmail.com>
2016-10-28 11:19:23 +0500

    updated

---
+++ Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace SEGP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.UOB = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(562, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Students List";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(137, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "PAT List";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(567, 88);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(69, 17);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Assigned";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(830, 458);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Assign";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(141, 88);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(37, 17);
+            this.checkBox2.TabIndex = 6;
+            this.checkBox2.Text = "All";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(142, 454);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Auto assign";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Name
+            // 
+            this.Name.HeaderText = "Name";
+            this.Name.Name = "Name";
+            this.Name.ReadOnly = true;
+            // 
+            // UOB
+            // 
+            this.UOB.HeaderText = "UOB";
+            this.UOB.Name = "UOB";
+            this.UOB.ReadOnly = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.UOB,
+            this.Name,
+            this.Column1});
+            this.dataGridView1.Location = new System.Drawing.Point(567, 138);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(342, 274);
+            this.dataGridView1.TabIndex = 8;
+            this.dataGridView1.CellMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_CellMouseClick);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Status";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToOrderColumns = true;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column4,
+            this.dataGridViewTextBoxColumn2,
+            this.Column2,
+            this.Column3});
+            this.dataGridView2.Location = new System.Drawing.Point(140, 138);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.ReadOnly = true;
+            this.dataGridView2.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView2.Size = new System.Drawing.Size(400, 274);
+            this.dataGridView2.TabIndex = 9;
+            this.dataGridView2.CellMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView2_CellMouseClick);
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "ID";
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.HeaderText = "Name";
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            this.dataGridViewTextBoxColumn2.ReadOnly = true;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Status";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "No.of Students";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(971, 506);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn UOB;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
+using System.Collections;
+
+namespace SEGP
+{
+    public partial class Form1 : Form
+    {
+        MySqlConnection myConnection = new MySqlConnection("SERVER=localhost;DATABASE=segp1;UID=root;Password=");
+        List<Student> st_list = new List<Student>();
+
+        List<PAT> pat_list = new List<PAT>();
+        int UOB1 ;
+        String SStatus = null;
+        int alloc;
+        int PID;
+        String PStatus=null;
+        MySqlDataReader reader = null;
+        public void connect()
+        {
+            st_list = null;
+            st_list = new List<Student>();
+            pat_list = null;
+            pat_list = new List<PAT>();
+
+            MySqlCommand command = new MySqlCommand();
+            myConnection.Open();
+            command = myConnection.CreateCommand();
+            command.CommandText = "SELECT* from students";
+            reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                Student stt = new Student();
+                stt.UOB = reader.GetInt32(0);
+                stt.name = reader.GetString(1);
+                stt.status = reader.GetString(6);
+            
+                st_list.Add(stt);
+
+            }
+            reader.Close();
+
+
+            command.CommandText = "SELECT * from pat";
+
+            reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                PAT p = new PAT();
+                p.ID = reader.GetInt32(0);
+                p.name = reader.GetString(1);
+                p.allocations = reader.GetInt16(4);
+                p.status = reader.GetString(5);
+                pat_list.Add(p);
+
+            }
+            reader.Close();
+            myConnection.Close();
+
+            
+        }
+        public void update()
+        {
+            myConnection.Open();
+            MySqlCommand command = new MySqlCommand();
+            command = myConnection.CreateCommand();
+
+            int counter = 0;
+            Student st;
+            foreach (PAT p in pat_list)
+            {
+                int ID = p.ID;
+                int alloc = p.allocations;
+                String status = p.status;
+                if (status.Equals("Full") && alloc < 8 && counter < st_list.Count)
+                {
+                    for (int i = alloc; i <= 8 && i <= st_list.Count; i++)
+                    {
+
+                        st = st_list[counter];
+                        if (st.status.Equals("Not Assigned"))
+                        {
+                            counter++;
+                            int a = i + 1;
+                            Console.WriteLine(st.UOB + " " + st.status);
+                            String s1 = "Update students SET Status= 'Assigned' WHERE UOB=" + st.UOB + ";";
+                            String s2 = "Update pat SET Allocations=" + a + " WHERE ID=" + ID + ";";
+                            String s3 = "INSERT into allocations (PAT_ID,UOB) values(" + ID + "," + st.UOB + ");";
+                            command.CommandText = s3;
+                            command.ExecuteNonQuery();
+                            command.CommandText = s1;
+                            command.ExecuteNonQuery();
+                            command.CommandText = s2;
+                            command.ExecuteNonQuery();
+
+                        }
+                    }
+                    if ((status.Equals("Partial") || status.Equals("Part")) && alloc < 4 && counter < st_list.Count)
+                    {
+
+                        for (int i = alloc; i <= 4 && i <= st_list.Count; i++)
+                        {
+                                st = st_list[counter];
+                                if (st.status.Equals("Not Assigned"))
+                                {
+                                    counter++;
+
+                                    String s1 = "Update students SET status= Assigned WHERE UOB=" + st.UOB + ";";
+                                    String s2 = "Update pat SET No.of_allocations=" + i + 1 + " WHERE ID=" + ID + ";";
+                                    String s3 = "INSERT into allocations (PAT_ID,UOB) values(" + ID + "," + st.UOB + ");";
+                                    command.CommandText = s1;
+                                    command.ExecuteNonQuery();
+                                    command.CommandText = s2;
+                                    command.ExecuteNonQuery();
+                                    command.CommandText = s3;
+                                    command.ExecuteNonQuery();
+
+                                
+                            }
+                        }
+                    }
+                }
+            }
+            myConnection.Close();            
+    }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    
+      
+        private void button1_Click(object sender, EventArgs e)
+        {
+       
+          
+                Console.WriteLine("hello");
+                myConnection.Open();
+                MySqlCommand command = new MySqlCommand();
+                command = myConnection.CreateCommand();
+
+          
+                if(((PStatus.Equals("Partial") && alloc<4)||(PStatus.Equals("Full") && alloc < 8)) && SStatus.Equals("Not Assigned"))
+                {
+                 
+                    String s = "insert into allocations values("+PID+","+UOB1+")";
+                    command.CommandText = s;
+                    command.ExecuteNonQuery();
+                    String s1 = "Update students set status ='Assigned' where uob=" +UOB1;
+                    command.CommandText = s1;
+                    command.ExecuteNonQuery();
+                    String s2 = "Update pat set Allocations = "+(alloc+1)+" where ID = "+PID;
+                    command.CommandText = s2;
+                    command.ExecuteNonQuery();
+                    myConnection.Close();
+                    connect();
+
+
+                }
+            else
+            {
+                Console.WriteLine("select student and pat to assign");
+            }
+                
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            connect();
+            update();
+
+            dataGridView1.DataSource = null;
+            dataGridView1.Rows.Clear();
+            dataGridView1.Refresh();
+            dataGridView2.DataSource = null;
+            dataGridView2.Rows.Clear();
+            dataGridView2.Refresh();
+            int count = 0;
+
+            foreach (PAT pt in pat_list)
+            {
+                if (pat_list.Count >= dataGridView2.RowCount)
+                {
+                    if (checkBox2.Checked)
+                    {
+                        dataGridView2.Rows.Add();
+
+                        dataGridView2.Rows[count].Cells[0].Value = pt.ID;
+                        dataGridView2.Rows[count].Cells[1].Value = pt.name;
+                        dataGridView2.Rows[count].Cells[2].Value = pt.status;
+                        dataGridView2.Rows[count].Cells[3].Value = pt.allocations;
+                        count++;
+                    }
+                    else if ((pt.status.Equals("Full") && pt.allocations < 8) || (pt.status.Equals("Part") && pt.allocations < 4))
+                    {
+                        dataGridView2.Rows.Add();
+                        dataGridView2.Rows[count].Cells[0].Value = pt.ID;
+                        dataGridView2.Rows[count].Cells[1].Value = pt.name;
+                        dataGridView2.Rows[count].Cells[2].Value = pt.status;
+                        dataGridView2.Rows[count].Cells[3].Value = pt.allocations;
+                        count++;
+                    }
+                }
+
+            }
+            count = 0;
+            foreach (Student st in st_list)
+            {
+                Console.WriteLine(st.name);
+                if (st_list.Count >= dataGridView1.RowCount)
+                {
+                    if (checkBox1.Checked)
+                    {
+                        dataGridView1.Rows.Add();
+                        dataGridView1.Rows[count].Cells[0].Value = st.UOB;
+                        dataGridView1.Rows[count].Cells[1].Value = st.name;
+                        dataGridView1.Rows[count].Cells[2].Value = st.status;
+                       
+                        count++;
+                    }
+                    else if (!st.status.Equals("Assigned"))
+                    {
+                        dataGridView1.Rows.Add();
+                        dataGridView1.Rows[count].Cells[0].Value = st.UOB;
+                        dataGridView1.Rows[count].Cells[1].Value = st.name;
+                        dataGridView1.Rows[count].Cells[2].Value = st.status;
+
+                        count++;
+                    }
+                }
+            }
+        }
+
+        private void dataGridView1_CellMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            int r = e.RowIndex;
+            //Console.WriteLine(dataGridView1.Rows[r].Cells[2].ToString());
+            UOB1 =Convert.ToInt32( dataGridView1.Rows[r].Cells[0].Value);
+            SStatus = dataGridView1.Rows[r].Cells[2].Value.ToString();
+        }
+
+        private void dataGridView2_CellMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            int r = e.RowIndex;
+            PID = Convert.ToInt32(dataGridView2.Rows[r].Cells[0].Value);
+            PStatus =dataGridView2.Rows[r].Cells[2].Value.ToString();
+            alloc = Convert.ToInt32(dataGridView2.Rows[r].Cells[3].Value.ToString());
+            
+
+
+        }
+    }
+
+    public class Student
+    {
+
+        public int UOB { get; set; }
+        public String status { get; set; }
+        public String name { get; set; }
+
+    }
+    public class PAT
+    {
+        public int ID { get; set; }
+
+        public int allocations { get; set; }
+
+        public String status { get; set; }
+
+        public String name { get; set; }
+    }
+}
